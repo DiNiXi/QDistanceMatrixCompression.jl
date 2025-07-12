30b92ff7a54a42d91205fe55af953d9cf26ed164
Dimitris Floros <flojim4@gmail.com>
2025-07-12 15:48:54 -0400

    fix

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ makedocs(;
     authors="Dimitris Floros, Nikos Pitsianis, Xiaobai Sun",
     sitename="QDistanceMatrixCompression.jl",
     format=Documenter.HTML(;
-        canonical="https://fcdimitr.github.io/QDistanceMatrixCompression.jl",
+        canonical="https://dinixi.github.io/QDistanceMatrixCompression.jl",
         edit_link="main",
         assets=String[],
     ),
@@ -18,6 +18,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/fcdimitr/QDistanceMatrixCompression.jl",
+    repo="github.com/DiNiXi/QDistanceMatrixCompression.jl",
     devbranch="main",
 )
